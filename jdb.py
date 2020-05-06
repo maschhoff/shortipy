af71e416115a4c00ec1361c3a5038cfe918f4d27
Mathias Aschhoff <web@little-devil.de>
2020-05-06 10:37:34 +0200

    Add files via upload

--- jdb.py
+++ jdb.py
@@ -8,14 +8,21 @@ db - Helpers
 """
 
 import json
+import os
 
 def loadDB():
-    #TODO if not exist create!
-    res={}
-    with open('./data/db.json', 'r') as fp:
-        res = json.load(fp)
-    return res  
+        if os.path.isfile('./data/db.json'):
+                res={}
+                with open('./data/db.json', 'r') as fp:
+                        res = json.load(fp)
+                return res 
+        else:
+                dict={}
+                dict["shortlinks"]={"1234":"https://github.com/maschhoff/shortipy"}
+                writeDB(dict)
+                return dict
+	  
 
 def writeDB(config):
-    with open('./data/db.json', 'w') as fp:
-	    json.dump(config, fp)
+        with open('./data/db.json', 'w') as fp:
+                json.dump(config, fp)
